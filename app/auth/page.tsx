5223d0892e315bfbd7c25d807e6d49d84c3292a8
Deiridre Kimberley <deirdre.kimberleyy@gmail.com>
2026-08-28 11:15:06 -0700

    Fix production auth redirect

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -18,7 +18,7 @@ export default function AuthPage() {
         password,
         options: {
           emailRedirectTo:
-            "http://localhost:3000/household",
+  `${window.location.origin}/household`,
         },
       });
 
